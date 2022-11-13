76fc43974f743a4ffb2ff1b26bcfcb87c36ab6ec
Karim Khasan <karimhasan3879@gmail.com>
2022-11-14 01:22:18 +0300

    fix: apply suggested changes

--- src/ru/croc/task7/Task7.java
+++ src/ru/croc/task7/Task7.java
@@ -5,37 +5,28 @@ import ru.croc.task7.core.ChessPosition;
 import ru.croc.task7.core.KnightMoveChecker;
 import ru.croc.task7.exceptions.IllegalMoveException;
 
+import java.util.Arrays;
+
 public class Task7 {
+
+	/***
+	 * Program validates moves of knight passed as command line arguments
+	 * @param args completed moves recorded in standard notation
+	 */
 	public static void main(String @NotNull [] args) {
-		if (args.length == 0) {
-			System.out.println("OK");
-			return;
-		}
-
-		boolean isCorrect = true;
+		ChessPosition[] moves = getMoves(args);
 		try {
-			CheckAllMoves(args);
-		}
-		catch (IllegalMoveException e) {
-			isCorrect = false;
-			System.out.println(e.getMessage());
-		}
-
-		if (isCorrect) {
+			KnightMoveChecker.checkAllMoves(moves);
 			System.out.println("OK");
+		} catch (IllegalMoveException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
-	private static void CheckAllMoves(String @NotNull [] args)
-		throws IllegalMoveException {
-		var knightMoveChecker = new KnightMoveChecker();
-
-		ChessPosition curPosition = ChessPosition.parse(args[0]);
-		for (int i = 1; i < args.length; ++i) {
-			ChessPosition prevPosition = curPosition;
-			curPosition = ChessPosition.parse(args[i]);
-
-			knightMoveChecker.check(prevPosition, curPosition);
-		}
+	private static ChessPosition @NotNull [] getMoves(String[] moveNotations) {
+		return Arrays
+			.stream(moveNotations)
+			.map(ChessPosition::parse)
+			.toArray(ChessPosition[]::new);
 	}
 }
--- src/ru/croc/task7/core/KnightMoveChecker.java
+++ src/ru/croc/task7/core/KnightMoveChecker.java
@@ -1,10 +1,18 @@
 package ru.croc.task7.core;
 
+import org.jetbrains.annotations.NotNull;
 import ru.croc.task7.exceptions.IllegalMoveException;
 
 public class KnightMoveChecker {
 	private static final int MAX_SINGLE_DELTA = 2, SUM_DELTA = 3;
-	public void check(ChessPosition from, ChessPosition to) throws IllegalMoveException {
+
+	public static void checkAllMoves(ChessPosition @NotNull [] moves) throws IllegalMoveException {
+		for (int i = 1; i < moves.length; ++i) {
+			checkMove(moves[i - 1], moves[i]);
+		}
+	}
+
+	public static void checkMove(ChessPosition from, ChessPosition to) throws IllegalMoveException {
 		int deltaX = getDeltaX(from, to);
 		int deltaY = getDeltaY(from, to);
 
@@ -13,15 +21,15 @@ public class KnightMoveChecker {
 		}
 	}
 
-	private int getDeltaX(ChessPosition from, ChessPosition to) {
+	private static int getDeltaX(ChessPosition from, ChessPosition to) {
 		return Math.abs(to.rank() - from.rank());
 	}
 
-	private int getDeltaY(ChessPosition from, ChessPosition to) {
+	private static int getDeltaY(ChessPosition from, ChessPosition to) {
 		return Math.abs(to.file() - from.file());
 	}
 
-	private boolean hasInvalidDeltas(int deltaX, int deltaY) {
+	private static boolean hasInvalidDeltas(int deltaX, int deltaY) {
 		return deltaX > MAX_SINGLE_DELTA || deltaY > MAX_SINGLE_DELTA
 			|| deltaX + deltaY != SUM_DELTA;
 	}
