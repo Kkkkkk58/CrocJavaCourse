c2535e26e52f760fd25827f35cb5a94f38518176
Karim Khasan <karimhasan3879@gmail.com>
2022-12-18 20:35:21 +0300

    fix: read filename from cl args

--- src/ru/croc/task17/Task17.java
+++ src/ru/croc/task17/Task17.java
@@ -12,13 +12,23 @@ public class Task17 {
 	private static final String CONNECTION_STRING = "jdbc:h2:tcp://localhost/~/test";
 	private static final String USER = "sa";
 	private static final String PASSWORD = "";
-	private static final String FILEPATH = "src/ru/croc/task17/resources/data.csv";
-	public static void main(String[] a)
+
+	/**
+	 * An entrypoint for task17 solution example
+	 * @param args first argument is the name of .csv file with data
+	 * @throws Exception in case of invalid number of parameters or problems with db connection
+	 */
+	public static void main(String[] args)
 		throws Exception {
+		if (args.length != 1) {
+			throw new IllegalArgumentException("Invalid number of input parameters");
+		}
+
+		final String filePath = args[0];
 		try (Connection connection = DriverManager.
 			getConnection(CONNECTION_STRING, USER, PASSWORD)) {
 			new ShopSchemaCreator().createSchema(connection);
-			BufferedReader csvStream = new BufferedReader(new FileReader(FILEPATH));
+			BufferedReader csvStream = new BufferedReader(new FileReader(filePath));
 			var orders = new ShopCsvReader().getFromCsv(csvStream);
 			new ShopDbSeeder().seedDb(connection, orders);
 		}
